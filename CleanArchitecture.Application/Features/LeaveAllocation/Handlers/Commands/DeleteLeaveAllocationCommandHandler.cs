5bb18a1d3acfc36d3cfbc8d337dd4460346b1bcc
senhimadri <senhimadri.dev@gmail.com>
2023-10-08 10:33:48 +0600

    Cruid Completed

---
+++ CleanArchitecture.Application/Features/LeaveAllocation/Handlers/Commands/DeleteLeaveAllocationCommandHandler.cs
@@ -0,0 +1,29 @@
+﻿using AutoMapper;
+using CleanArchitecture.Application.Features.LeaveAllocation.Requests.Commands;
+using CleanArchitecture.Application.Parsistence.Contracts;
+using MediatR;
+
+namespace CleanArchitecture.Application.Features.LeaveAllocation.Handlers.Commands;
+
+public class DeleteLeaveAllocationCommandHandler :IRequestHandler<DeleteLeaveAllocationCommand>
+{
+    private readonly ILeaveAllocationRepository _repository;
+    private readonly IMapper _mapper;
+
+    public DeleteLeaveAllocationCommandHandler(ILeaveAllocationRepository repository, IMapper mapper)
+    {
+        _repository = repository;
+        _mapper = mapper;
+    }
+
+    public async Task<Unit> Handle(DeleteLeaveAllocationCommand request, CancellationToken cancellationToken)
+    {
+        var leaveallocation = await _repository.GetAsync(request.Id);
+        if (leaveallocation is null)
+            throw new Exception("Not Found");
+
+        await _repository.DeleteAsync(leaveallocation.Id);
+
+        return Unit.Value;
+    }
+}
---
+++ CleanArchitecture.Application/Features/LeaveAllocation/Requests/Commands/DeleteLeaveAllocationCommand.cs
@@ -0,0 +1,8 @@
+﻿using MediatR;
+
+namespace CleanArchitecture.Application.Features.LeaveAllocation.Requests.Commands;
+
+public class DeleteLeaveAllocationCommand :IRequest
+{
+    public int Id { get; set; }
+}

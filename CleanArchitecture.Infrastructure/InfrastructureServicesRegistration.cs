0c6f79d859d8ffee440adff0d4017510df5e95d5
senhimadri <senhimadri.dev@gmail.com>
2023-10-10 17:50:31 +0600

    Infrastructure Software

---
+++ CleanArchitecture.Infrastructure/InfrastructureServicesRegistration.cs
@@ -0,0 +1,19 @@
+﻿using CleanArchitecture.Application.Contracts.Infrastructure;
+using CleanArchitecture.Application.Models;
+using CleanArchitecture.Infrastructure.Mail;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CleanArchitecture.Infrastructure
+{
+    public static class InfrastructureServicesRegistration
+    {
+
+        public static IServiceCollection ConfigureInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.Configure<EmailSettings>(configuration.GetSection("EmailSettings"));
+            services.AddTransient<IEmailSender, EmailSender>();
+            return services;
+        }
+    }
+}

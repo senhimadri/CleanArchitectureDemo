1c13937ba6327cee8abba851a022205a54e42777
senhimadri <senhimadri.dev@gmail.com>
2023-10-10 08:21:25 +0600

    ConfigurePersistenceServices for dependency injections

---
+++ CleanArchitecture.Persistence/PersistenceServicesRegistration.cs
@@ -0,0 +1,24 @@
+﻿using CleanArchitecture.Application.Parsistence.Contracts;
+using CleanArchitecture.Persistence.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CleanArchitecture.Persistence;
+
+public static class PersistenceServicesRegistration
+{
+    public static IServiceCollection ConfigurePersistenceServices(this IServiceCollection services,IConfiguration configuration)
+    {
+        services.AddDbContext<LeaveManagmentDbContext>(options =>
+            options.UseSqlite(configuration.GetConnectionString("LeaveManagmentConnectionString")));
+
+        services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+
+        services.AddScoped<ILeaveTypeRepository, LeaveTypeRepository>();
+        services.AddScoped<ILeaveRequestRepository, LeaveRequestRepository>();
+        services.AddScoped<ILeaveAllocationRepository, LeaveAllocationRepository>();
+
+        return services;
+    }
+}

47b7d8718a068a9634fc9d8141f3cd0ff25a2664
senhimadri <senhimadri.dev@gmail.com>
2023-10-25 17:06:32 +0600

    LeaveManagmentDbContextFactory update

---
+++ CleanArchitecture.Persistence/LeaveManagmentDbContextFactory.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+
+namespace CleanArchitecture.Persistence;
+
+public class LeaveManagmentDbContextFactory : IDesignTimeDbContextFactory<LeaveManagmentDbContext>
+{
+    public LeaveManagmentDbContext CreateDbContext(string[] args)
+    {
+        IConfigurationRoot configuration = new ConfigurationBuilder()
+           .SetBasePath(Directory.GetCurrentDirectory())
+           .AddJsonFile("appsettings.json")
+           .Build();
+
+        var builder = new DbContextOptionsBuilder<LeaveManagmentDbContext>();
+
+        var connectionString = configuration.GetConnectionString("LeaveManagementConnectionString");
+
+        builder.UseSqlite(connectionString);
+
+        return new LeaveManagmentDbContext(builder.Options);
+    }
+}

410de589f3cbd915de4147ffc8b25a23e0a50874
senhimadri <senhimadri.dev@gmail.com>
2023-11-17 21:16:20 +0600

    Git Upload

---
+++ CleanArchitecture.Persistence/Configurations/LeaveTypeConfiguration.cs
@@ -0,0 +1,31 @@
+﻿using CleanArchitecture.Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchitecture.Persistence.Configurations;
+
+public class LeaveTypeConfiguration : IEntityTypeConfiguration<LeaveType>
+{
+    public void Configure(EntityTypeBuilder<LeaveType> builder)
+    {
+        builder.HasData(
+            new LeaveType
+            {
+                Id = 1,
+                DefaultDays=10,
+                Name="Vacation"
+            },
+            new LeaveType
+            {
+                Id = 1,
+                DefaultDays = 12,
+                Name = "Sick"
+            }
+        );
+    }
+}

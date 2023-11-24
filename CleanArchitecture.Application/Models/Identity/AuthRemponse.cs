ade1cc2233e1e62a3f038509424279f4d7c048dd
senhimadri <senhimadri.dev@gmail.com>
2023-11-24 14:19:35 +0600

    Setting Up the DTO For Identity

---
+++ CleanArchitecture.Application/Models/Identity/AuthRemponse.cs
@@ -0,0 +1,10 @@
+﻿namespace CleanArchitecture.Application.Models.Identity;
+
+public class AuthRemponse
+{
+    public string Id { get; set; }
+    public string UserName { get; set; }
+    public string Email { get; set; }
+    public string Token { get; set; }
+}
+
---
+++ CleanArchitecture.Application/Models/Identity/RegistrationResponse.cs
@@ -0,0 +1,7 @@
+﻿namespace CleanArchitecture.Application.Models.Identity;
+
+public class RegistrationResponse
+{
+    public string UserId { get; set; }
+}
+
